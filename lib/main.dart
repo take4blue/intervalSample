0ddb700146151008ff24ad1bcc6ffd499925157b
take4 <46077789+take4blue@users.noreply.github.com>
2022-08-15 09:22:47 +0900

    初期化

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// A Counter example implemented with riverpod
+
+void main() {
+  runApp(
+    // Adding ProviderScope enables Riverpod for the entire project
+    const ProviderScope(child: MyApp()),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Home());
+  }
+}
+
+/// Providers are declared globally and specifies how to create a state
+final counterProvider = StateProvider((ref) => 0);
+
+class Home extends ConsumerWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter example')),
+      body: Center(
+        // Consumer is a widget that allows you reading providers.
+        // You could also use the hook "ref.watch(" if you uses flutter_hooks
+        child: Consumer(builder: (context, ref, _) {
+          final count = ref.watch(counterProvider.state).state;
+          ref.listen<int>(
+            counterProvider,
+            (previous, next) => debugPrint("$previous $next"),
+          );
+          debugPrint((count as String).length.toString());
+          return Text('$count');
+        }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // The read method is an utility to read a provider without listening to it
+        onPressed: () {
+          ref.watch(counterProvider.state).state++;
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
