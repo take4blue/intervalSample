88abbe541c6bab3bb13afe6d399e45ee8dba8da7
take4 <46077789+take4blue@users.noreply.github.com>
2022-09-09 11:54:57 +0900

    インターバル処理を別クラス化

---
+++ lib/intervaltimer.dart
@@ -0,0 +1,145 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+import 'dart:isolate';
+import 'dart:ui';
+
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:flutter/foundation.dart';
+
+/// 呼び出すアラームの種別
+enum AlarmType {
+  /// Android Alarm Manager
+  alarm,
+
+  /// Timer
+  timer,
+}
+
+/// 間隔でタイマー処理[awake]を実行する
+class IntervalTimer {
+  IntervalTimer(this.awake)
+      : _type = canUse(AlarmType.alarm) ? AlarmType.alarm : AlarmType.timer;
+
+  /// 発動しているアラームの種別
+  AlarmType _type;
+
+  /// Alarm Managerの識別番号
+  static int kAlarmId = 0;
+
+  /// Timer設定時のオブジェクト
+  Timer? _timer;
+
+  /// アラームを発動しているか
+  bool _isStarted = false;
+
+  bool get isStarted => _isStarted;
+
+  /// 発動間隔
+  Duration _interval = const Duration(seconds: 20);
+
+  /// 発動間隔変更
+  set interval(int second) {
+    _interval = Duration(seconds: second);
+  }
+
+  /// UI側で実行する関数
+  void Function() awake;
+
+  /// アラームの種別取得
+  AlarmType get type => _type;
+
+  /// 次回のアラーム設定をする
+  FutureOr<void> oneShot() async {
+    _isStarted = true;
+    switch (_type) {
+      case AlarmType.alarm:
+        await AndroidAlarmManager.oneShot(
+          _interval,
+          kAlarmId,
+          callback,
+          exact: true,
+          wakeup: true,
+          allowWhileIdle: true,
+        );
+        break;
+      case AlarmType.timer:
+        _timer = Timer(_interval, _awake);
+        break;
+    }
+  }
+
+  /// 発動を止める
+  FutureOr<void> stop() async {
+    if (_isStarted) {
+      switch (_type) {
+        case AlarmType.alarm:
+          await AndroidAlarmManager.cancel(kAlarmId);
+          break;
+        case AlarmType.timer:
+          if (_timer != null) {
+            _timer!.cancel();
+          }
+          break;
+      }
+      _isStarted = false;
+    }
+  }
+
+  /// アラームの再設定。
+  /// [type]が設定不可の場合は、種別変更せずに再スタートする。
+  FutureOr<void> restart(AlarmType type) async {
+    await stop();
+    if (canUse(type)) {
+      _type = type;
+    }
+    await oneShot();
+  }
+
+  Future<void> _awake() async {
+    awake();
+    oneShot();
+  }
+
+  /// ポートの初期化
+  void initState() {
+    port.listen((_) async => await _awake());
+  }
+
+  /// callbackからUIへのポート
+  static SendPort? uiSendPort;
+
+  /// アラームの起動関数。
+  /// 起動された後、ポート経由でポートリッスン中の[_awake]が呼び出される。
+  static Future<void> callback() async {
+    uiSendPort ??= IsolateNameServer.lookupPortByName(isolateName);
+    uiSendPort?.send(null);
+  }
+
+  /// The name associated with the UI isolate's [SendPort].
+  static const String isolateName = 'isolate';
+
+  /// A port used to communicate from a background isolate to the UI isolate.
+  static final ReceivePort port = ReceivePort();
+
+  /// パッケージの初期化関数
+  static FutureOr<void> initialize() async {
+    if (!kIsWeb && Platform.isAndroid) {
+      final ans = await AndroidAlarmManager.initialize();
+      debugPrint("initialize $ans");
+      IsolateNameServer.registerPortWithName(
+        port.sendPort,
+        isolateName,
+      );
+    }
+  }
+
+  /// 指定したアラーム種別がこの機種で使用可能かどうかの判断
+  static bool canUse(AlarmType type) {
+    switch (type) {
+      case AlarmType.alarm:
+        return (!kIsWeb && Platform.isAndroid) ? true : false;
+      case AlarmType.timer:
+        return true;
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -7,41 +7,23 @@
 import 'dart:async';
 import 'dart:developer' as developer;
 import 'dart:io' show Platform;
-import 'dart:isolate';
-import 'dart:ui';
 
-import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:optimize_battery/optimize_battery.dart';
 
-/// The name associated with the UI isolate's [SendPort].
-const String isolateName = 'isolate';
-
-/// A port used to communicate from a background isolate to the UI isolate.
-final ReceivePort port = ReceivePort();
+import 'intervaltimer.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (!kIsWeb && Platform.isAndroid) {
-    AndroidAlarmManager.initialize();
-  }
+  IntervalTimer.initialize();
   if (Platform.isAndroid) {
     final isIgnored = await OptimizeBattery.isIgnoringBatteryOptimizations();
     if (!isIgnored) {
       await OptimizeBattery.stopOptimizingBatteryUsage();
     }
   }
-
-  // Register the UI isolate's SendPort to allow for communication from the
-  // background isolate.
-  IsolateNameServer.registerPortWithName(
-    port.sendPort,
-    isolateName,
-  );
-
   runApp(const AlarmManagerExampleApp());
 }
 
@@ -67,15 +49,6 @@ class _AlarmHomePage extends StatefulWidget {
   _AlarmHomePageState createState() => _AlarmHomePageState();
 }
 
-/// 呼び出すアラームの種別
-enum AlarmType {
-  /// Android Alarm Manager
-  alarm,
-
-  /// Timer
-  timer,
-}
-
 class _AlarmHomePageState extends State<_AlarmHomePage> {
   /// リストに表示するタイムスタンプ情報
   final _list = <String>[];
@@ -86,70 +59,29 @@ class _AlarmHomePageState extends State<_AlarmHomePage> {
   /// 前回発動してからの経過時間を求めるための前回の時刻
   var _prev = DateTime.now();
 
-  /// 発動しているアラームの種別
-  AlarmType _type = AlarmType.alarm;
+  late IntervalTimer timer = IntervalTimer(awake);
 
-  /// Alarm Managerの識別番号
-  static int kAlarmId = 0;
-
-  /// Timer設定時のオブジェクト
-  Timer? _timer;
-
-  /// アラームを発動しているか
-  bool isStarted = false;
-
-  /// 発動間隔
-  Duration interval = const Duration(seconds: 20);
+  FutureOr<void> awake() async {
+    developer.log('awake');
+    final now = DateTime.now();
+    final span = now.difference(_prev);
+    setState(() {
+      _list.insert(0, "${formatter.format(now.toLocal())}  ${span.inSeconds}");
+    });
+    _prev = now;
+  }
 
   late TextEditingController _controller;
 
-  /// 次回のアラーム設定をする
-  FutureOr<void> _oneShot() async {
-    isStarted = true;
-    switch (_type) {
-      case AlarmType.alarm:
-        await AndroidAlarmManager.oneShot(
-          interval,
-          kAlarmId,
-          callback,
-          exact: true,
-          wakeup: true,
-          allowWhileIdle: true,
-        );
-        break;
-      case AlarmType.timer:
-        _timer = Timer(interval, _awake);
-        break;
-    }
-  }
-
-  /// 発動を止める
-  FutureOr<void> _stop() async {
-    if (isStarted) {
-      switch (_type) {
-        case AlarmType.alarm:
-          await AndroidAlarmManager.cancel(kAlarmId);
-          break;
-        case AlarmType.timer:
-          if (_timer != null) {
-            _timer!.cancel();
-          }
-          break;
-      }
-      isStarted = false;
-      setState(() {});
-    }
-  }
-
   /// アラームの再設定
   FutureOr<void> _restart(AlarmType type) async {
-    await _stop();
+    timer.restart(type);
     setState(() {
-      _type = type;
       _list.clear();
+      final now = DateTime.now();
+      _list.insert(0, formatter.format(now.toLocal()));
+      _prev = DateTime.now();
     });
-    _prev = DateTime.now();
-    await _oneShot();
   }
 
   @override
@@ -157,8 +89,7 @@ class _AlarmHomePageState extends State<_AlarmHomePage> {
     super.initState();
     _controller = TextEditingController();
     _controller.text = "20";
-    port.listen((_) async => await _awake());
-    _type = (!kIsWeb && Platform.isAndroid) ? AlarmType.alarm : AlarmType.timer;
+    timer.initState();
   }
 
   @override
@@ -167,33 +98,8 @@ class _AlarmHomePageState extends State<_AlarmHomePage> {
     super.dispose();
   }
 
-  /// アラームからの呼び出し
-  Future<void> _awake() async {
-    developer.log('awake');
-    final now = DateTime.now();
-    final span = now.difference(_prev);
-    setState(() {
-      _list.insert(0, "${formatter.format(now.toLocal())}  ${span.inSeconds}");
-    });
-    _prev = now;
-    _oneShot();
-  }
-
-  /// callbackからUIへのポート
-  static SendPort? uiSendPort;
-
-  /// アラームの起動関数。
-  /// 起動された後、ポート経由でポートリッスン中の[_awake]が呼び出される。
-  static Future<void> callback() async {
-    developer.log('Alarm fired!');
-
-    // This will be null if we're running in the background.
-    uiSendPort ??= IsolateNameServer.lookupPortByName(isolateName);
-    uiSendPort?.send(null);
-  }
-
   /// ラジオボタンのコールバック
-  void setRadio(AlarmType? select) {
+  void _setRadio(AlarmType? select) {
     _restart(select ?? AlarmType.alarm);
   }
 
@@ -209,14 +115,15 @@ class _AlarmHomePageState extends State<_AlarmHomePage> {
             children: [
               Radio<AlarmType>(
                 value: AlarmType.alarm,
-                groupValue: _type,
-                onChanged: (!kIsWeb && Platform.isAndroid) ? setRadio : null,
+                groupValue: timer.type,
+                onChanged:
+                    IntervalTimer.canUse(AlarmType.alarm) ? _setRadio : null,
               ),
               const Text("Alarm"),
               Radio<AlarmType>(
                 value: AlarmType.timer,
-                groupValue: _type,
-                onChanged: setRadio,
+                groupValue: timer.type,
+                onChanged: _setRadio,
               ),
               const Text("Timer"),
               Expanded(
@@ -227,7 +134,7 @@ class _AlarmHomePageState extends State<_AlarmHomePage> {
                   onEditingComplete: () {
                     final value = int.tryParse(_controller.text);
                     if (value != null) {
-                      interval = Duration(seconds: value);
+                      timer.interval = value;
                     }
                     final FocusScopeNode currentScope = FocusScope.of(context);
                     if (!currentScope.hasPrimaryFocus &&
@@ -239,13 +146,14 @@ class _AlarmHomePageState extends State<_AlarmHomePage> {
               ),
               TextButton(
                   onPressed: () {
-                    if (isStarted) {
-                      _stop();
+                    if (timer.isStarted) {
+                      timer.stop();
+                      setState(() {});
                     } else {
-                      _restart(_type);
+                      _restart(timer.type);
                     }
                   },
-                  child: Text(isStarted ? "Stop" : "Start")),
+                  child: Text(timer.isStarted ? "Stop" : "Start")),
             ],
           ),
           Expanded(
